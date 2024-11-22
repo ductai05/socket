3a9cf519fcf3c121a71a89cc13110bcd3c80c2b6
Duc Tai Dinh <ductaidt05@gmail.com>
2024-11-22 13:01:00 +0700

    update receive.h

--- receive.h
+++ receive.h
@@ -16,14 +16,101 @@
 #include <vector>
 #include <map>
 #include <regex>
+
 using namespace std;
 
-string getCurrentDateTime();
-string createFileBatGetID(string userPass);
-void getID(string filename);
-bool readIDMail(int &orderNow, int &idMailNow);
-bool getNewestMail(int orderNow, string userPass);
-bool extractInfo(const string &subject, string &responseType, string &numTask, string &time);
+string getCurrentDateTime() {
+    std::time_t t = std::time(nullptr);
+    std::tm* now = std::localtime(&t);
+
+    std::ostringstream oss;
+    oss << std::put_time(now, "%Y-%m-%d %H:%M:%S");
+    return oss.str();
+}
+
+string createFileBatGetID(string userPass){
+    const char* filename = "getId.bat";
+
+    ofstream batFile(filename);
+    if (!batFile) {
+        cerr << "Can't create file: " << filename << endl;
+        return "";
+    }
+
+    batFile << "@echo off\n";
+    batFile << "if not \"%minimized%\"==\"\" goto :minimized\n";
+    batFile << "set minimized=true\n";
+    batFile << "start /min cmd /c \"%~dpnx0\"\n";
+    batFile << "goto :EOF\n";
+    batFile << ":minimized\n";
+    batFile << "curl -u \"" << userPass << "\" --ssl-reqd \"pop3s://pop.gmail.com:995\" -e \"UIDL\" -o id.txt\n";
+    batFile.close();
+    cout << "File " << filename << " created successfully." << endl;
+    return filename;
+}
+
+void getID(string filename){
+    system(filename.c_str());
+    cout << "get new ID:: ok\n";
+}
+
+bool readIDMail(int &orderNow, int &idMailNow){
+    ifstream idFile("id.txt");
+    string line, data;
+    while(!idFile.eof()){
+        getline(idFile, line);
+        if (!line.empty()) data = line;
+    }
+    stringstream ss(data);
+    string order, id;
+    ss >> order >> id;
+    idFile.close();
+    if (idMailNow != stoi(id)){
+        idMailNow = stoi(id);
+        orderNow = stoi(order);
+        return true;
+    }
+    return false;
+}
+
+bool getNewestMail(int orderNow, string userPass){
+    const char* filename = "getMail.bat";
+    ofstream batFile(filename);
+    if (!batFile) {
+        cerr << "Can't create file: " << filename << endl;
+        return false;
+    }
+
+    batFile << "@echo off\n";
+    batFile << "if not \"%minimized%\"==\"\" goto :minimized\n";
+    batFile << "set minimized=true\n";
+    batFile << "start /min cmd /c \"%~dpnx0\"\n";
+    batFile << "goto :EOF\n";
+    batFile << ":minimized\n";
+    batFile << "curl -v pop3s://pop.gmail.com:995/" << orderNow << " --ssl-reqd ^\n";
+    batFile << "  --connect-timeout 20 ^\n";
+    batFile << "  --max-time 60 ^\n";
+    batFile << "  -u \"" << userPass << "\" ^\n";
+    batFile << "  -o latest_email.eml\n";
+    batFile.close();
+
+    system(filename);
+    return true;
+}
+
+bool extractInfo(const string &subject, string &responseType, string &numTask, string &time) {
+    regex pattern(R"(\[(response|request)_(\d+)\]: (\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}))");
+    smatch matches;
+
+    if (regex_search(subject, matches, pattern)) {
+        responseType = matches[1]; // 'response' or 'request'
+        numTask = matches[2];        // numtask
+        time = matches[3];      // time
+        return true;
+    }
+
+    return false;
+}
 
 // Map for base64
 map<char, int> base64_map = {
@@ -40,9 +127,132 @@ map<char, int> base64_map = {
     {'8', 60}, {'9', 61}, {'+', 62}, {'/', 63}, {'=', -1}
 };
 
-vector<unsigned char> base64_decode(const std::string &in);
-void saveFile(const std::string &filename, const std::vector<unsigned char> &data);
-void readLatestMail();
-void autoGetMail();
+// Ham giai ma base64
+vector<unsigned char> base64_decode(const std::string &in) {
+    vector<unsigned char> out;
+    int val = 0, valb = -8;
+    for (unsigned char c : in) {
+        if (base64_map.find(c) == base64_map.end()) continue; // Skip ki tu ko phai base64
+        val = (val << 6) + base64_map[c];
+        valb += 6;
+        if (valb >= 0) {
+            out.push_back((val >> valb) & 0xFF);
+            valb -= 8;
+        }
+    }
+    return out;
+}
+
+void saveFile(const std::string &filename, const std::vector<unsigned char> &data) {
+    string directory = "attachment";
+    string command = "mkdir " + directory;
+    system(command.c_str());
+    string fullPath = directory + "/" + filename;
+
+    ofstream out(fullPath, std::ios::binary);
+    out.write(reinterpret_cast<const char*>(data.data()), data.size());
+    out.close();
+}
+
+void readLatestMail(){
+    string fileName = "latest_email.eml";
+
+    int k = 0;
+    while(!ifstream(fileName).good()){
+        Sleep(1000); k++;
+        if (k == 5) return;
+    } // dam bao latest_email.eml da duoc tao
+
+    ifstream inFile(fileName);
+    string line;
+    string subject, body, responseType, numTask, time;
+    string fileAttachmentName;
+    // getSubject
+    while(getline(inFile, line)){
+        if (line.find("Subject:") != string::npos) {
+            subject = line.substr(line.find(":") + 1);
+            subject.erase(0, subject.find_first_not_of(" \t"));
+            subject.erase(subject.find_last_not_of(" \t") + 1);
+            break; 
+        }
+    }
+    if (subject.find("[response_") || subject.find("[request_")){
+        bool check = false;
+        regex pattern(R"(\[(response|request)_(\d+)\]: (\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}))");
+        smatch matches;
+
+        if (regex_search(subject, matches, pattern)) {
+            responseType = matches[1]; // 'response' or 'request'
+            numTask = matches[2];      // numtask
+            time = matches[3];         // time
+            check = true;
+        }
+        if (check) cout << "Type: " + responseType + ", NumTask: " + numTask + ", Timestamp: " + time << "\n";
+    } 
+
+    // getBody
+    while(getline(inFile, line)){
+        if (line.find("[rep]") != string::npos || line.find("[task]") != string::npos) {
+            body = line.substr(line.find("]") + 1);
+            body.erase(0, body.find_first_not_of(" \t"));
+            body.erase(body.find_last_not_of(" \t") + 1);
+            break; 
+        }
+    }
+    cout << body << "\n";
+
+    // getFileAttachmentName
+    while(getline(inFile, line)){
+        if (line.find("filename=\"") != string::npos) {
+            fileAttachmentName = line.substr(line.find("\"") + 1);
+            fileAttachmentName = fileAttachmentName.substr(0, fileAttachmentName.size() - 1);
+
+            getline(inFile, line); 
+            //getline(inFile, line); // skip 1 lines
+            break; 
+        }
+    }
+    cout << fileAttachmentName << "\n";
+
+    // get base64 
+    string base64_data;
+    int i = 1;
+    while (getline(inFile, line)) {
+        if (line == "--boundary--") break;
+        base64_data += line;
+    }
+    
+    // Giai ma base64
+    vector<unsigned char> decoded_data = base64_decode(base64_data);
+
+    // Luu file
+    saveFile(fileAttachmentName, decoded_data);
+}
+
+void autoGetMail(){
+    string daytime = getCurrentDateTime();
+    cout << "Start listen at: " << daytime << "\n";
+
+    string userPass = "ai23socket@gmail.com:nhrr llaa ggzb yzbj";
+    string filebat = createFileBatGetID(userPass);
+    getID(filebat);
+    while(!std::ifstream("id.txt").good()) Sleep(1000); // dam bao id.txt da duoc tao
+    cout << "exist id.txt\n"; // wait for id.txt created
+
+    int idMailNow = 0, orderNow = 1;
+    while(true){
+        getID(filebat);
+        while(!std::ifstream("id.txt").good()) Sleep(1000);
+        if (readIDMail(orderNow, idMailNow)){
+            if (getNewestMail(orderNow, userPass)){
+                cout << "get new mail:: ok : " << idMailNow << "\n";
+                readLatestMail();
+            }
+        }
+        
+        Sleep(5000);
+    }
+    // remove("latest_email.eml");
+}
 
 #endif
